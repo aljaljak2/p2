10a37885f274212dd7cf9171ea99296a8621fe69
aljaljak2 <146694584+aljaljak2@users.noreply.github.com>
2024-03-21 05:32:04 +0100

    Update MojaDrugaKlasa.java

--- src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
@@ -2,6 +2,6 @@ package ba.unsa.etf.rpr;
 
 public class MojaDrugaKlasa {
     public static void main(String[] args) {
-        System.out.println("Pozdrav!");
+        System.out.println("Pozdrav svim neradnicima");
     }
 }
