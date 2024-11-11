b39b5ea133b281a6f1e328008db2202db1d678db
aljaljak2 <146694584+aljaljak2@users.noreply.github.com>
2024-11-11 11:37:37 +0100

    ds

--- src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
+++ src/ba/unsa/etf/rpr/MojaDrugaKlasa.java
@@ -3,5 +3,6 @@ package ba.unsa.etf.rpr;
 public class MojaDrugaKlasa {
     public static void main(String[] args) {
         System.out.println("Pozdrav svim neradnicima");
+		 System.out.println("Pozdrav svim neradnicima");
     }
 }
